e9b252a0a1317ec680a68646dc7d18c8138db2ab
Sankethaaa <sankethak77@gmail.com>
2026-05-18 11:41:17 +0530

    Create HiveScreen.kt

---
+++ ui/ui/HiveScreen.kt
@@ -0,0 +1,9 @@
+package com.example.madhumarga.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HiveScreen() {
+    Text("Hive Details")
+}
